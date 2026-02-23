d831d2f976b288282046a711323ac95c4809302c
Ali-ahsan35 <syedahsan748@gmail.com>
2026-02-23 15:08:29 +0600

    db logger updated

--- utils/db_logger.py
+++ utils/db_logger.py
@@ -1,4 +1,7 @@
+# utils/db_logger.py
+import threading
 from automation.models import StepResult, NetworkLog
+
 
 class DBLogger:
 
@@ -6,21 +9,35 @@ class DBLogger:
         self.journey_run = journey_run
 
     def log_step(self, step_number, test_case, passed, comment="", screenshot_path="", url=""):
-        step = StepResult.objects.create(
-            journey_run=self.journey_run,
-            step_number=step_number,
-            test_case=test_case,
-            passed=passed,
-            comment=comment,
-            screenshot_path=screenshot_path,
-            url=url
-        )
-        return step
+        result = {}
+
+        def _save():
+            step = StepResult.objects.create(
+                journey_run=self.journey_run,
+                step_number=step_number,
+                test_case=test_case,
+                passed=passed,
+                comment=comment,
+                screenshot_path=screenshot_path,
+                url=url
+            )
+            result['step'] = step
+
+        thread = threading.Thread(target=_save)
+        thread.start()
+        thread.join()
+        return result.get('step')
 
     def log_network(self, step_result, log_type, message):
-        NetworkLog.objects.create(
-            journey_run=self.journey_run,
-            step_result=step_result,
-            log_type=log_type,
-            message=message
-        )
+
+        def _save():
+            NetworkLog.objects.create(
+                journey_run=self.journey_run,
+                step_result=step_result,
+                log_type=log_type,
+                message=message
+            )
+
+        thread = threading.Thread(target=_save)
+        thread.start()
+        thread.join()
